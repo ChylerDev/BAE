513ee66f522d93e7e15a727ebb9d225b5f95edce
Chyler Morrison <chyler.morrison@gmail.com>
2019-05-09 00:35:19 -0700

    Added BandPass filter

---
+++ src/Modifiers/BandPass.cpp
@@ -0,0 +1,114 @@
+/*! ****************************************************************************
+\file             BandPass.cpp
+\author           Chyler Morrison
+\par    Email:    contact\@chyler.info
+\par    Project:  AudioEngine
+
+\copyright        Copyright © 2018 Chyler
+*******************************************************************************/
+
+// Include Files                          //////////////////////////////////////
+
+#include <cmath>
+
+#include "BandPass.hpp"
+
+// Private Macros                         //////////////////////////////////////
+
+// Private Enums                          //////////////////////////////////////
+
+// Private Objects                        //////////////////////////////////////
+
+// Private Function Declarations          //////////////////////////////////////
+
+// Public Functions                       //////////////////////////////////////
+
+namespace Modifiers
+{
+
+  BandPass::BandPass(float f, float Q) :
+    m_CentralFrequency(f), m_Quality(Q)
+  {
+    Reset();
+  }
+
+  void BandPass::SetFrequency(float f)
+  {
+    m_CentralFrequency = f;
+
+    Reset();
+  }
+
+  void BandPass::SetQuality(float Q)
+  {
+    m_Quality = Q;
+
+    Reset();
+  }
+
+  StereoData_t BandPass::FilterSample(StereoData_t const & x)
+  {
+    StereoData_t y;
+
+    std::get<0>(y) = float(m_A0 * (std::get<0>(x)-std::get<0>(m_X2)) + m_B1 * std::get<0>(m_Y1) - m_B2 * std::get<0>(m_Y2));
+    std::get<1>(y) = float(m_A0 * (std::get<1>(x)-std::get<1>(m_X2)) + m_B1 * std::get<1>(m_Y1) - m_B2 * std::get<1>(m_Y2));
+
+    m_Y2 = m_Y1;
+    m_Y1 = y;
+    m_X2 = m_X1;
+    m_X1 = x;
+
+    return y;
+  }
+
+} // namespace Modifiers
+
+// Private Functions                      //////////////////////////////////////
+
+namespace Modifiers
+{
+
+  void BandPass::Reset()
+  {
+    #if 1
+      double fL, fH;
+
+      double a = 1;
+      double b = -m_CentralFrequency/m_Quality;
+      double c = -m_CentralFrequency*m_CentralFrequency;
+
+      double fL_q1 = (-b + std::sqrt(b*b - 4*a*c)) / (2*a);
+      double fL_q2 = (-b - std::sqrt(b*b - 4*a*c)) / (2*a);
+
+      fL = (fL_q1 > 0 ? fL_q1 : fL_q2);
+      fH = fL + b;
+
+      ////////
+
+      double thetaL, thetaH, aL, aH, bL, bH;
+
+      thetaL = std::tan(PI*fL*INC_RATE);
+      thetaH = std::tan(PI*fH*INC_RATE);
+
+      aL = 1 / (1+thetaL);
+      aH = 1 / (1+thetaH);
+
+      bL = (1-thetaL) / (1+thetaL);
+      bH = (1-thetaH) / (1+thetaH);
+
+      m_A0 = (1-aL) * aH;
+
+      m_B1 = bL + bH;
+      m_B2 = bL * bH;
+    #else
+      double theta = std::tan(PI*m_CentralFrequency*INC_RATE);
+      double thetasq = theta*theta;
+      double norm = 1.0/(m_Quality * thetasq + theta + m_Quality);
+
+      m_A0 = theta*norm;
+      m_B1 = 2 * m_Quality * (1-thetasq)*norm;
+      m_B2 = (m_Quality*thetasq - theta+m_Quality)*norm;
+    #endif
+  }
+
+} // namespace Modifiers
---
+++ src/Modifiers/BandPass.hpp
@@ -0,0 +1,72 @@
+/*! ****************************************************************************
+\file             BandPass.hpp
+\author           Chyler Morrison
+\par    Email:    contact\@chyler.info
+\par    Project:  AudioEngine
+
+\copyright        Copyright © 2018 Chyler
+*******************************************************************************/
+
+#ifndef __BAND_PASS_HPP
+#define __BAND_PASS_HPP
+
+// Include Files                ////////////////////////////////////////////////
+
+#include "../Engine.hpp"
+
+// Public Macros                ////////////////////////////////////////////////
+
+// Forward References           ////////////////////////////////////////////////
+
+// Public Enums                 ////////////////////////////////////////////////
+
+// Public Objects               ////////////////////////////////////////////////
+
+namespace Modifiers
+{
+
+  /*! **************************************************************************
+  \brief
+  *****************************************************************************/
+  class BandPass
+  {
+  private:
+
+    // Members              ///////////////////////
+
+    double m_CentralFrequency;
+    double m_Quality;
+    double m_A0, m_B1, m_B2;
+    StereoData_t m_X1, m_X2, m_Y1, m_Y2;
+
+  public:
+
+    // Con-/De- structors   ///////////////////////
+
+    BandPass(float f, float Q = 1);
+
+    // Operators            ///////////////////////
+
+    // Accossors/Mutators   ///////////////////////
+
+    void SetFrequency(float f);
+
+    void SetQuality(float Q);
+
+    // Functions            ///////////////////////
+
+    StereoData_t FilterSample(StereoData_t const & x);
+
+  private:
+
+    // Functions                  ///////////////////////
+
+    void Reset(void);
+
+  }; // class BandPass
+
+} // namespace Modifiers
+
+// Public Functions             ////////////////////////////////////////////////
+
+#endif // __BAND_PASS_HPP
