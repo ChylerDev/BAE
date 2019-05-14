49bae4f61b8c78427289842ef215a5717a3ca243
Chyler Morrison <chyler.morrison@gmail.com>
2019-05-13 23:07:17 -0700

    Added envelope detector + NOTE: this is a low-quality filter, can definitely be improved (not that I know how)

---
+++ src/Engine/Modifiers/Envelope.hpp
@@ -0,0 +1,71 @@
+/*! ****************************************************************************
+\file             Envelope.hpp
+\author           Chyler Morrison
+\par    Email:    contact\@chyler.info
+\par    Project:  AudioEngine
+
+\copyright        Copyright © 2018 Chyler
+*******************************************************************************/
+
+#ifndef __STUB_HPP
+#define __STUB_HPP
+
+// Include Files                ////////////////////////////////////////////////
+
+#include "../Engine.hpp"
+
+#include "Base.hpp"
+
+// Public Macros                ////////////////////////////////////////////////
+
+// Forward References           ////////////////////////////////////////////////
+
+// Public Enums                 ////////////////////////////////////////////////
+
+// Public Objects               ////////////////////////////////////////////////
+
+namespace AudioEngine
+{
+namespace Modifier
+{
+
+  /*! **************************************************************************
+  \brief
+  *****************************************************************************/
+  class EnvelopeFollower : public Base
+  {
+  private:
+
+    // Members              ///////////////////////
+
+    double m_AU, m_BU;
+    double m_AD, m_BD;
+    StereoData_t m_X1, m_Y1;
+
+  public:
+
+    // Con-/De- structors   ///////////////////////
+
+    EnvelopeFollower(float fd, float fu);
+    virtual ~EnvelopeFollower();
+
+    // Operators            ///////////////////////
+
+    virtual StereoData_t FilterSample(StereoData_t const & x);
+
+    // Accossors/Mutators   ///////////////////////
+
+    // Functions            ///////////////////////
+
+  private:
+
+    // Functions                  ///////////////////////
+
+  }; // class EnvelopeFollower
+
+} // namespace Modifier
+} // namespace AudioEngine
+
+// Public Functions             ////////////////////////////////////////////////
+
+#endif // __STUB_HPP
