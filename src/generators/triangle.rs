9ab165470acc415cd6a3048a4cf43a9817d232c1
Chyler Morrison <contact@chyler.info>
2019-12-29 15:17:37 -0500

    Added more generators

---
+++ src/generators/triangle.rs
@@ -0,0 +1,48 @@
+//! # Triangle
+//! 
+//! A triangle wave generator.
+
+use super::*;
+
+/// Struct for generating triangle waves at a given frequency.
+pub struct Triangle {
+    irate: MathT,
+    inc: SampleT,
+}
+
+impl FreqMod for Triangle {
+    fn new(f: MathT) -> Self {
+        Triangle {
+            irate: 4.0 * f * INV_SAMPLE_RATE,
+            inc: 0.0,
+        }
+    }
+
+    fn set_frequency(&mut self, f: MathT) {
+        self.irate = 4.0 * f * INV_SAMPLE_RATE;
+    }
+
+    fn get_frequency(&self) -> MathT {
+        self.irate / (4.0 * INV_SAMPLE_RATE)
+    }
+}
+
+impl Generator for Triangle {
+    fn process(&mut self) -> StereoData {
+        let y = StereoData::from_mono(self.inc);
+
+        self.inc += self.irate as SampleT;
+
+        if self.inc >= 1.0 || self.inc <= -1.0 {
+            self.irate = -self.irate;
+
+            self.inc = if self.inc >= 1.0 {
+                2.0 - self.inc
+            } else {
+                -2.0 - self.inc
+            };
+        }
+
+        y
+    }
+}
--- tests/generators.rs
+++ tests/generators.rs
@@ -4,49 +4,54 @@ extern crate ocae;
 mod tests {
     #[test]
     fn test_noise() {
-        use ocae::generators::Generator;
         use ocae::generators::noise::*;
 
-        let mut n = Noise::new();
-
-        let mut t = ocae::TrackT::new();
-        
-        for _ in 0..ocae::SAMPLE_RATE {
-            t.push(n.process());
-        }
-
-        ocae::tools::write_wav(t, String::from(".junk/generators/noise.wav"));
+        run_generator(&mut Noise::new(), &String::from("noise.wav"));
     }
 
     #[test]
     fn test_sawtooth() {
-        use ocae::generators::Generator;
+        use ocae::generators::FreqMod;
         use ocae::generators::sawtooth::*;
 
-        let mut s = Sawtooth::new(440.0);
-
-        let mut t = ocae::TrackT::new();
-
-        for _ in 0..ocae::SAMPLE_RATE {
-            t.push(s.process());
-        }
-
-        ocae::tools::write_wav(t, String::from(".junk/generators/sawtooth.wav"))
+        run_generator(&mut Sawtooth::new(440.0), &String::from("sawtooth.wav"));
     }
 
     #[test]
     fn test_sine() {
-        use ocae::generators::Generator;
+        use ocae::generators::FreqMod;
         use ocae::generators::sine::*;
 
-        let mut s = Sine::new(440.0);
+        run_generator(&mut Sine::new(440.0), &String::from("sine.wav"));
+    }
 
+    #[test]
+    fn test_square() {
+        use ocae::generators::FreqMod;
+        use ocae::generators::square::*;
+
+        run_generator(&mut Square::new(440.0), &String::from("square.wav"));
+    }
+
+    #[test]
+    fn test_triangle() {
+        use ocae::generators::FreqMod;
+        use ocae::generators::triangle::*;
+
+        run_generator(&mut Triangle::new(440.0), &String::from("triangle.wav"));
+    }
+
+    fn run_generator(g: &mut impl ocae::generators::Generator, file:&String)
+    {
         let mut t = ocae::TrackT::new();
-
+    
         for _ in 0..ocae::SAMPLE_RATE {
-            t.push(s.process());
+            t.push(g.process());
         }
+    
+        let mut f = String::from(".junk/generators/");
+        f.push_str(file.as_str());
 
-        ocae::tools::write_wav(t, String::from(".junk/generators/sine.wav"));
+        ocae::tools::write_wav(t, f);
     }
 }
