f55947a62dcd4fea5945ac536846ed7e190c86f8
Chyler Morrison <contact@chyler.info>
2020-01-07 12:23:01 -0800

    Fixed modifiers tests

--- tests/modifiers.rs
+++ tests/modifiers.rs
@@ -4,32 +4,40 @@ extern crate ocae;
 mod tests {
 	#[test]
 	fn test_adsr() {
-		use ocae::modifiers::adsr::*;
+		use ocae::modifiers::{*, adsr::*};
 		use std::time::Duration;
 
 		let mut a = ADSR::new(
-			Duration::from_secs_f64(0.0625),
+			Duration::from_secs_f64(0.03125),
 			Duration::from_secs_f64(0.125),
-			ocae::linear_db(0.75),
+			ocae::linear_db(0.5),
 			Duration::from_secs_f64(0.5)
 		);
 
-		run_modifier(
-			&mut a,
-			Box::new(|i|{if i == ocae::SAMPLE_RATE/2 { a.release(); } }),
-			String::from("adsr.wav")
-		);
-	}
-
-	fn run_modifier(m: &mut impl ocae::modifiers::Modifier, mut c: Box<dyn FnMut(u64)>, file:String)
-	{
 		use ocae::generators::{Generator, FreqMod, sine::*};
 
 		let mut g = Sine::new(440.0);
 		let mut t = ocae::TrackT::new();
 
 		for i in 0..ocae::SAMPLE_RATE {
-			(*c)(i);
+			if i == ocae::SAMPLE_RATE/2 {
+				a.release();
+			}
+
+			t.push(a.process(g.process()));
+		}
+
+		let f = String::from(".junk/modifiers/adsr.wav");
+		ocae::tools::write_wav(t, f).unwrap();
+	}
+
+	fn run_modifier(m: &mut impl ocae::modifiers::Modifier, file:String) {
+		use ocae::generators::{Generator, FreqMod, sine::*};
+
+		let mut g = Sine::new(440.0);
+		let mut t = ocae::TrackT::new();
+
+		for _ in 0..ocae::SAMPLE_RATE {
 			t.push(m.process(g.process()));
 		}
 
