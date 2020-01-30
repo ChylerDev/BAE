a5dd9217357f0a0f3e7b0aed32241bb75fa31945
Chyler Morrison <contact@chyler.info>
2020-01-30 12:49:20 -0800

    Working on blocks, added generic lifetime

--- src/sounds/block.rs
+++ src/sounds/block.rs
@@ -1,33 +1,34 @@
 use super::*;
 use std::rc::Rc;
 
-pub type Gen = Rc<dyn generators::Generator>;
-pub type Mod = Rc<dyn modifiers::Modifier>;
+pub type Gen<'a> = Rc<&'a mut dyn generators::Generator>;
+pub type Mod<'a> = Rc<&'a mut dyn modifiers::Modifier>;
 pub type IntBase = dyn FnMut(StereoData, StereoData) -> StereoData;
 pub type Int = Box<IntBase>;
 
-pub struct Block {
-	g: Gen,
-	m: Mod,
+pub struct Block<'a> {
+	g: Gen<'a>,
+	m: Mod<'a>,
 	i: Int,
 	input: StereoData,
 }
 
-impl Block {
-	pub fn new<T, U>(g: T, m: U, i: Int) -> Block 
-		where T: 'static + generators::Generator + Clone,
-		      U: 'static + modifiers::Modifier + Clone
+impl<'a> Block<'a> {
+	pub fn new<T, U>(g: &'a mut T, m: &'a mut U, i: Int) -> Block<'a>
+		where T: generators::Generator + Clone,
+		      U: modifiers::Modifier + Clone
 	{
-		Block {
+		Block::<'a> {
 			g: Rc::new(g),
 			m: Rc::new(m),
 			i,
 			input: StereoData::default(),
 		}
 	}
-	pub fn default_interactor<T, U>(g: T, m: U) -> Block 
-		where T: 'static + generators::Generator + Clone,
-		      U: 'static + modifiers::Modifier + Clone
+
+	pub fn default_interactor<T, U>(g: &'a mut T, m: &'a mut U) -> Block<'a>
+		where T: generators::Generator + Clone,
+		      U: modifiers::Modifier + Clone
 	{
 		Block {
 			g: Rc::new(g),
@@ -37,17 +38,17 @@ impl Block {
 		}
 	}
 
-	pub fn get_g(&self) -> &Gen {
+	pub fn get_g(&self) -> &'a Gen {
 		&self.g
 	}
-	pub fn get_m(&self) -> &Mod {
+	pub fn get_m(&self) -> &'a Mod {
 		&self.m
 	}
 
-	pub fn get_g_mut(&mut self) -> &mut Gen {
+	pub fn get_g_mut(&mut self) -> &'a mut Gen {
 		&mut self.g
 	}
-	pub fn get_m_mut(&mut self) -> &mut Mod {
+	pub fn get_m_mut(&mut self) -> &'a mut Mod {
 		&mut self.m
 	}
 
