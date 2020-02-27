d36df716f6b0a0a83d75d12f1cb2912df1d3bf28
Chyler Morrison <contact@chyler.info>
2020-02-26 20:34:52 -0800

    Started work on tests for the sound module

--- tests/sounds.rs
+++ tests/sounds.rs
@@ -2,5 +2,18 @@ extern crate bae_rs;
 
 #[cfg(test)]
 mod tests {
-	
+	#[test]
+	fn test_blocks() {
+		use bae_rs::sounds::{basic_block::*, block::*};
+	}
+
+	#[test]
+	fn test_simple_sounds() {
+
+	}
+
+	#[test]
+	fn test_complex_sounds() {
+
+	}
 }
