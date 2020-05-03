b9742eef38c88e13e0893fa61770773913c10939
Chyler Morrison <contact@chyler.info>
2020-05-02 22:02:34 -0700

    Changed the name of TrackT to SampleTrackT

--- src/lib.rs
+++ src/lib.rs
@@ -71,7 +71,7 @@ pub type MathT = f64;
 /// Type to calculate samples with.
 pub type SampleT = f32;
 /// Shorthand for a vector containing sample data.
-pub type TrackT = Vec<SampleT>;
+pub type SampleTrackT = Vec<SampleT>;
 
 /// The sampling rate the engine is set to run at.
 pub const SAMPLE_RATE:u64 = 48_000;
